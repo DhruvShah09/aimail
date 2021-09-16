e88c6d320bcc0f1537efa793bd2cecdfeacec477
Dhruv Shah <90000344+DhruvShah09@users.noreply.github.com>
2021-09-16 08:35:47 -0400

    Update environment.go
    Removed keys

--- secure/environment.go
+++ secure/environment.go
@@ -5,8 +5,8 @@ import (
 )
 
 func SecureEnvironment() {
-	os.Setenv("OPENAI_KEY", "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")
-	os.Setenv("EMAIL_USER", "testporson@gmail.com")
-	os.Setenv("EMAIL_PWD", "Jenko123!")
-	os.Setenv("WEATHER_KEY", "84e7da84ac5041069e2151349210809")
+	os.Setenv("OPENAI_KEY", "")
+	os.Setenv("EMAIL_USER", "")
+	os.Setenv("EMAIL_PWD", "")
+	os.Setenv("WEATHER_KEY", "")
 }
