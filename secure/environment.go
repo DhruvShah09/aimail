ac50bb1c69bf810065f70cd3474614f94ab80c6e
Dhruv Shah <90000344+DhruvShah09@users.noreply.github.com>
2021-09-17 09:34:11 -0400

    Update environment.go

--- secure/environment.go
+++ secure/environment.go
@@ -4,6 +4,14 @@ import (
 	"os"
 )
 
+//Insert Relavent API KEYS and Passwords Here
+
+/*
+	OPENAI_KEY: Corresponds to OPEN AI API key. Make sure to rotate before production
+	EMAIL_USER: Email address, GMAIL (not sure if others work)
+	EMAIL_PWD: Password for above email
+	WEATHER_KEY: API Key for weather API -- currently unsure of what API will be used.
+*/
 func InitSecureEnvironment() {
 	os.Setenv("OPENAI_KEY", "")
 	os.Setenv("EMAIL_USER", "")
