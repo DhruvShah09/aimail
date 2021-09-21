6420516c95e7f50dc9a42a0513152feb61f93314
Dhruv Shah <90000344+DhruvShah09@users.noreply.github.com>
2021-09-20 21:48:30 -0400

    Refactored string concatenation using string arry

--- weather/serv.go
+++ weather/serv.go
@@ -1,6 +1,16 @@
 package weather
 
-func GeneratePrompt() []string {
-	a := []string{"Write a poem about the weather on a day with ", "% humidity, a high of ", " F, a low of 82 ", ", and rain chance of ", "%:"}
-	return a
+type Coords struct {
+	Latitude  float64
+	Longitude float64
+}
+
+func GetWeather(c string) {
+
+}
+
+func GeneratePrompt(hu string, hi string, lo string, r string) string {
+	a := []string{"Write a poem about the weather on a day with ", "% humidity, a high of ", " F, a low of ", ", and rain chance of ", "%:"}
+	prompt := a[0] + hu + a[1] + hi + a[2] + lo + a[3] + r + a[4]
+	return prompt
 }
