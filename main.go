b4745dedb93062cf96a4289b5ef033a89752a3da
Dhruv Shah <90000344+DhruvShah09@users.noreply.github.com>
2021-09-30 12:29:44 -0400

    main updates, refactor api call

--- main.go
+++ main.go
@@ -8,13 +8,21 @@ import (
 
 	//Absolute path of development environment packges, refactor for production
 
-	"github.com/dhruvshah/go_crash_course/aimail/cdn"
+	"fmt"
+
 	"github.com/dhruvshah/go_crash_course/aimail/secure" //secure init
+	"github.com/dhruvshah/go_crash_course/aimail/weather"
 )
 
 func main() {
 	secure.InitSecureEnvironment()
-	x := "Write a poem:"
-	response := cdn.GetPoem(x)
-	cdn.SendEmail(response)
+	//x := "Write a poem:"
+	//response := cdn.GetPoem(x)
+	//cdn.SendEmail(response)
+	location := []string{"33.44", "-94.04"}
+	if weather.GetWeather(location) {
+		fmt.Println("Retrieved Weather Data")
+	} else {
+		fmt.Println("Error collecting weather data")
+	}
 }
